256ada07bfc83427e59856a7984105312bb16fd3
drkaine <kevin.duret@jaguar-network.com>
2023-06-08 10:14:45 +0200

    Refactoring
    Création d'un composant commun pour les view head
    include du composant dans les views

--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Repas en avance</title>
-    </head>
+@include('composants.head')
     <body>
         <header>
             <a href="{{  route('accueil')  }}">Accueil</a>
