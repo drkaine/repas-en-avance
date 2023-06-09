72f62a1835ccec8d4f4ac9638bb430fa3b677aa2
drkaine <kevin.duret@jaguar-network.com>
2023-06-09 11:37:35 +0200

    Front
    Ajout des liens vers les pages ajout recette et tag
    dans le menu

--- resources/views/composants/menu.blade.php
+++ resources/views/composants/menu.blade.php
@@ -3,4 +3,6 @@
         <a href="{{  route('accueil')  }}">Accueil</a>
         <a href="{{  route('inscription')  }}">Inscription</a>
         <a href="{{  route('connexion')  }}">Connexion</a>
+        <a href="{{  route('ajout_tag')  }}">Ajout Tag</a>
+        <a href="{{  route('ajout_recette')  }}">Ajout Recette</a>
     </header>
