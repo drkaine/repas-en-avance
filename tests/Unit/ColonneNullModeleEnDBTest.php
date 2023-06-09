c752f2eb12f64a1521757126965923dc884bb308
drkaine <duretkevin@live.fr>
2023-06-09 22:41:20 +0200

    Test phase verte
    Vérification champs null

--- tests/Unit/ColonneNullModeleEnDBTest.php
+++ tests/Unit/ColonneNullModeleEnDBTest.php
@@ -85,4 +85,22 @@ class ColonneNullModeleEnDBTest extends TestCase
 
 		$this->assertDatabaseHas('recettes', $recette);
 	}
+
+	public function testDeLaTableRecetteChampsLien(): void
+	{
+		$recette = [
+			'temps_preparation' => 1,
+			'temps_cuisson' => 3,
+			'temps_repos' => 2,
+			'lien' => null,
+			'instruction' => 'Eplucher les carottes',
+			'description' => 'Recette simple et rapide',
+			'reference_livre' => 'Tous en cuisine page 12',
+			'nom' => 'Carotte simple',
+		];
+
+		Recette::factory()->create($recette);
+
+		$this->assertDatabaseHas('recettes', $recette);
+	}
 }
