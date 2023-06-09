291f931a234e277e8963b5331050fedd4b113a85
drkaine <duretkevin@live.fr>
2023-06-09 22:34:59 +0200

    Test phase verte
    Vérification champs null

---
+++ tests/Unit/ColonneNullModeleEnDBTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+/**
+ * @coversNothing
+ */
+class ColonneNullModeleEnDBTest extends TestCase
+{
+	use RefreshDatabase;
+
+	public function testDeLaTableUsersChampsEmailVerifiedAt(): void
+	{
+		$user = [
+			'nom' => 'Test user',
+			'email' => 'email@test.fr',
+			'password' => 'password',
+			'email_verified_at' => null,
+			'derniere_connexion' => '06-06-2023 06:06:06',
+		];
+
+		User::factory()->create($user);
+
+		unset($user['password']);
+
+		$this->assertDatabaseHas('users', $user);
+	}
+}
