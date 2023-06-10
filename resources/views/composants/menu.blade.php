bc83dda3a3a19a3405c8d3d78a08f544f9120a1d
drkaine <duretkevin@live.fr>
2023-06-10 21:23:05 +0200

    Front
    Modification du menu
    Ajout d'une condition à l'authentification du user pour certain lien

--- resources/views/composants/menu.blade.php
+++ resources/views/composants/menu.blade.php
@@ -1,8 +1,13 @@
 <body>
     <header>
         <a href="{{  route('accueil')  }}">Accueil</a>
-        <a href="{{  route('inscription')  }}">Inscription</a>
-        <a href="{{  route('connexion')  }}">Connexion</a>
-        <a href="{{  route('ajout_tag')  }}">Ajout Tag</a>
-        <a href="{{  route('ajout_recette')  }}">Ajout Recette</a>
+        @auth
+            <a href="{{  route('ajout_tag')  }}">Ajout Tag</a>
+            <a href="{{  route('ajout_recette')  }}">Ajout Recette</a>
+            <a href="{{  route('mon_compte')  }}">Mon compte</a>
+            <a href="{{  route('deconnexion')  }}">Déconnexion</a>
+        @else
+            <a href="{{  route('inscription')  }}">Inscription</a>
+            <a href="{{  route('connexion')  }}">Connexion</a>
+        @endauth
     </header>
