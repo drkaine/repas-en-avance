356bb7becb484b27a6d9cfd0a31ade590e5c3ae6
drkaine <duretkevin@live.fr>
2023-06-12 21:12:56 +0200

    Front
    Ajout du formulaire de modification de donnée du user

--- resources/views/mon_compte.blade.php
+++ resources/views/mon_compte.blade.php
@@ -3,5 +3,30 @@
         <div>
             <a href="{{ route('anonymisation_du_compte') }}">Supprimer mon compte</a>
         </div>
+
+        <form method="POST" action="{{ route('mon_compte') }}">
+            @csrf
+
+            <div>
+                <label for="email">Adresse e-mail</label>
+                <input id="email" type="email" name="email" value="{{  $user->email  }}" required autofocus>
+                @error('email')
+                    <span>{{ $message }}</span>
+                @enderror
+            </div>
+
+            <div>
+                <label for="nom">Nom</label>
+                <input id="nom" type="text" name="nom" value="{{  $user->nom  }}" required>
+                @error('nom')
+                    <span>{{ $message }}</span>
+                @enderror
+            </div>
+
+            <div>
+                <button type="submit">Mettre à jour ses informations</button>
+            </div>
+        </form>
+
     </body>
 </html>
