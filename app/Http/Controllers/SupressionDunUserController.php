d079581d934bee4d24906469bb363ed108f0ff4f
drkaine <kevin.duret@jaguar-network.com>
2023-06-12 10:42:14 +0200

    Test phase verte
    Ajout de la méthode d'anonymisation d'un user

--- app/Http/Controllers/SupressionDunUserController.php
+++ app/Http/Controllers/SupressionDunUserController.php
@@ -11,6 +11,16 @@ class SupressionDunUserController extends Controller
 {
 	public function anonymisation_du_compte(): RedirectResponse | Redirector
 	{
+		$user = auth()->user();
+
+		$user->nom = 'Anonyme';
+		$user->email = 'anonyme' . $user->id . '@anonyme.fr';
+		$user->derniere_connexion = null;
+		$user->password = bcrypt('anonyme');
+		$user->email_verified_at = null;
+
+		$user->save();
+
 		return redirect('/');
 	}
 }
---
+++ tests/Unit/AnonymisationUserTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+/**
+ * @coversNothing
+ */
+class AnonymisationUserTest extends TestCase
+{
+	use RefreshDatabase;
+
+	public function testAnonymisationUser(): void
+	{
+		$donnee_user = [
+			'nom' => 'Test user',
+			'email' => 'email@test.fr',
+			'password' => 'password',
+			'email_verified_at' => '2023-06-06 06:06:06',
+			'derniere_connexion' => '06-06-2023 06:06:06',
+		];
+
+		$user = User::factory()->create($donnee_user);
+
+		unset($donnee_user['password']);
+
+		$this->actingAs($user);
+
+		$this->get('/supression_du_compte');
+
+		$this->assertDatabaseMissing('users', $donnee_user);
+
+		$this->assertDatabaseHas('users', [
+			'email' => 'anonyme' . $user->id . '@anonyme.fr',
+			'nom' => 'Anonyme',
+			'derniere_connexion' => null,
+			'email_verified_at' => null,
+		]);
+	}
+}
