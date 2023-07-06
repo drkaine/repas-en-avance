5cd619635983e3c48cb570254ac64f3f11247771
drkaine <kevin.duret@jaguar-network.com>
2023-07-06 10:50:35 +0200

    Refactoring
    Utilisation du trait de modelDeTest pour ne pas répéter les variables

--- tests/Feature/ValidationFormulaire/AjoutRecetteTest.php
+++ tests/Feature/ValidationFormulaire/AjoutRecetteTest.php
@@ -6,6 +6,7 @@ namespace Tests\Feature\ValidationFormulaire;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Tests\Traits\ModelDeTestTrait;
 
 /**
  * @coversNothing
@@ -13,131 +14,85 @@ use Tests\TestCase;
 class AjoutRecetteTest extends TestCase
 {
 	use RefreshDatabase;
+	use ModelDeTestTrait;
+
+	private array $donnees_recette;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+
+		$this->donnees_recette = $this->donneesRecette();
+	}
 
 	public function testChampsNom(): void
 	{
-		$response = $this->post('/ajout_recette', [
-			'nom' => null,
-			'temps_preparation' => 1,
-			'temps_cuisson' => 2,
-			'temps_repos' => 3,
-			'lien' => 'https://ici.fr',
-			'instruction' => 'Eplucher les carottes',
-			'description' => 'Recette simple et rapide',
-			'reference_livre' => 'Tous en cuisine page 12',
-		]);
+		$this->donnees_recette['nom'] = null;
+
+		$response = $this->post('/ajout_recette', $this->donnees_recette);
 
 		$response->assertStatus(302);
 	}
 
 	public function testLongueurMaximumChampsNom(): void
 	{
-		$response = $this->post('/ajout_recette', [
-			'nom' => 'azertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv azertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv azertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv azertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv azertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv',
-			'temps_preparation' => 1,
-			'temps_cuisson' => 2,
-			'temps_repos' => 3,
-			'lien' => 'https://ici.fr',
-			'instruction' => 'Eplucher les carottes',
-			'description' => 'Recette simple et rapide',
-			'reference_livre' => 'Tous en cuisine page 12',
-		]);
+		$this->donnees_recette['nom'] = 'azertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv azertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv azertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv azertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv azertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv';
+
+		$response = $this->post('/ajout_recette', $this->donnees_recette);
 
 		$response->assertStatus(302);
 	}
 
 	public function testLongueurMinimumChampsNom(): void
 	{
-		$response = $this->post('/ajout_recette', [
-			'nom' => 'az',
-			'temps_preparation' => 1,
-			'temps_cuisson' => 2,
-			'temps_repos' => 3,
-			'lien' => 'https://ici.fr',
-			'instruction' => 'Eplucher les carottes',
-			'description' => 'Recette simple et rapide',
-			'reference_livre' => 'Tous en cuisine page 12',
-		]);
+		$this->donnees_recette['nom'] = 'az';
+
+		$response = $this->post('/ajout_recette', $this->donnees_recette);
 
 		$response->assertStatus(302);
 	}
 
 	public function testChampsTempsDePreparation(): void
 	{
-		$response = $this->post('/ajout_recette', [
-			'nom' => 'Carotte simple',
-			'temps_preparation' => null,
-			'temps_cuisson' => 2,
-			'temps_repos' => 3,
-			'lien' => 'https://ici.fr',
-			'instruction' => 'Eplucher les carottes',
-			'description' => 'Recette simple et rapide',
-			'reference_livre' => 'Tous en cuisine page 12',
-		]);
+		$this->donnees_recette['temps_preparation'] = null;
+
+		$response = $this->post('/ajout_recette', $this->donnees_recette);
 
 		$response->assertStatus(302);
 	}
 
 	public function testLongueurMinimumChampsLien(): void
 	{
-		$response = $this->post('/ajout_recette', [
-			'nom' => 'Carotte simple',
-			'temps_preparation' => 1,
-			'temps_cuisson' => 2,
-			'temps_repos' => 3,
-			'lien' => 'https://.fr',
-			'instruction' => 'Eplucher les carottes',
-			'description' => 'Recette simple et rapide',
-			'reference_livre' => 'Tous en cuisine page 12',
-		]);
+		$this->donnees_recette['lien'] = 'https://.fr';
+
+		$response = $this->post('/ajout_recette', $this->donnees_recette);
 
 		$response->assertStatus(302);
 	}
 
 	public function testLongueurMinimumChampsInstruction(): void
 	{
-		$response = $this->post('/ajout_recette', [
-			'nom' => 'Carotte simple',
-			'temps_preparation' => 1,
-			'temps_cuisson' => 2,
-			'temps_repos' => 3,
-			'lien' => 'https://ici.fr',
-			'instruction' => 'Eplu',
-			'description' => 'Recette simple et rapide',
-			'reference_livre' => 'Tous en cuisine page 12',
-		]);
+		$this->donnees_recette['instruction'] = 'Eplu';
+
+		$response = $this->post('/ajout_recette', $this->donnees_recette);
 
 		$response->assertStatus(302);
 	}
 
 	public function testLongueurMinimumChampsDescription(): void
 	{
-		$response = $this->post('/ajout_recette', [
-			'nom' => 'Carotte simple',
-			'temps_preparation' => 1,
-			'temps_cuisson' => 2,
-			'temps_repos' => 3,
-			'lien' => 'https://ici.fr',
-			'instruction' => 'Eplucher les carottes',
-			'description' => 'Rece',
-			'reference_livre' => 'Tous en cuisine page 12',
-		]);
+		$this->donnees_recette['description'] = 'Rece';
+
+		$response = $this->post('/ajout_recette', $this->donnees_recette);
 
 		$response->assertStatus(302);
 	}
 
 	public function testLongueurMinimumChampsReference(): void
 	{
-		$response = $this->post('/ajout_recette', [
-			'nom' => 'Carotte simple',
-			'temps_preparation' => 1,
-			'temps_cuisson' => 2,
-			'temps_repos' => 3,
-			'lien' => 'https://ici.fr',
-			'instruction' => 'Eplucher les carottes',
-			'description' => 'Recette simple et rapide',
-			'reference_livre' => 'Tous',
-		]);
+		$this->donnees_recette['reference_livre'] = 'Tous';
+
+		$response = $this->post('/ajout_recette', $this->donnees_recette);
 
 		$response->assertStatus(302);
 	}
--- tests/Feature/ValidationFormulaire/InscriptionTest.php
+++ tests/Feature/ValidationFormulaire/InscriptionTest.php
@@ -6,6 +6,7 @@ namespace Tests\Feature\ValidationFormulaire;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Tests\Traits\ModelDeTestTrait;
 
 /**
  * @coversNothing
@@ -13,139 +14,115 @@ use Tests\TestCase;
 class InscriptionTest extends TestCase
 {
 	use RefreshDatabase;
+	use ModelDeTestTrait;
+
+	private array $donnees_user;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+
+		$this->donnees_user = $this->donneesUser();
+
+		$this->donnees_user['email_verified_at'] = '2023-06-06 06:06:06';
+
+		$this->donnees_user['password_confirmation'] = 'password';
+	}
 
 	public function testChampsNom(): void
 	{
-		$response = $this->post('/inscription', [
-			'nom' => null,
-			'email' => 'email@test.fr',
-			'password' => 'password',
-			'email_verified_at' => '2023-06-06 06:06:06',
-			'password_confirmation' => 'password',
-		]);
+		$this->donnees_user['nom'] = null;
+
+		$response = $this->post('/inscription', $this->donnees_user);
 
 		$response->assertStatus(302);
 	}
 
 	public function testLongueurMaximumChampsNom(): void
 	{
-		$response = $this->post('/inscription', [
-			'nom' => 'azertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv azertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv',
-			'email' => 'email@test.fr',
-			'password' => 'password',
-			'email_verified_at' => '2023-06-06 06:06:06',
-			'password_confirmation' => 'password',
-		]);
+		$this->donnees_user['nom'] = 'aztrhgertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvaztrhgertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvaztrhgertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcvbnazertyuiopmlkjhgfdsqwxcv';
+
+		$response = $this->post('/inscription', $this->donnees_user);
 
 		$response->assertStatus(302);
 	}
 
 	public function testLongueurMinimumChampsNom(): void
 	{
-		$response = $this->post('/inscription', [
-			'nom' => 'az',
-			'email' => 'email@test.fr',
-			'password' => 'password',
-			'email_verified_at' => '2023-06-06 06:06:06',
-			'password_confirmation' => 'password',
-		]);
+		$this->donnees_user['nom'] = 'az';
+
+		$response = $this->post('/inscription', $this->donnees_user);
 
 		$response->assertStatus(302);
 	}
 
 	public function testChampsEmail(): void
 	{
-		$response = $this->post('/inscription', [
-			'nom' => 'Test user',
-			'email' => null,
-			'password' => 'password',
-			'email_verified_at' => '2023-06-06 06:06:06',
-			'password_confirmation' => 'password',
-		]);
+		$this->donnees_user['email'] = null;
+
+		$response = $this->post('/inscription', $this->donnees_user);
 
 		$response->assertStatus(302);
 	}
 
 	public function testLongueurMaximumChampsEmail(): void
 	{
-		$response = $this->post('/inscription', [
-			'nom' => 'Test user',
-			'email' => 'azertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuio@azertyuiopmlkjhgfdsqwxczertyuiopmlkjhgfdsqwxcv.fr',
-			'password' => 'password',
-			'email_verified_at' => '2023-06-06 06:06:06',
-			'password_confirmation' => 'password',
-		]);
+		$this->donnees_user['email'] = 'azertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuio';
+
+		$response = $this->post('/inscription', $this->donnees_user);
 
 		$response->assertStatus(302);
 	}
 
 	public function testLongueurMinimumChampsEmail(): void
 	{
-		$response = $this->post('/inscription', [
-			'nom' => 'Test user',
-			'email' => 'a@.fr',
-			'password' => 'password',
-			'email_verified_at' => '2023-06-06 06:06:06',
-			'password_confirmation' => 'password',
-		]);
+		$this->donnees_user['email'] = 'a@.fr';
+
+		$response = $this->post('/inscription', $this->donnees_user);
 
 		$response->assertStatus(302);
 	}
 
 	public function testFormatChampsEmail(): void
 	{
-		$response = $this->post('/inscription', [
-			'nom' => 'Test user',
-			'email' => 'emailestfr',
-			'password' => 'password',
-			'email_verified_at' => '2023-06-06 06:06:06',
-			'password_confirmation' => 'password',
-		]);
+		$this->donnees_user['email'] = 'emailestfr';
+
+		$response = $this->post('/inscription', $this->donnees_user);
 
 		$response->assertStatus(302);
 	}
 
 	public function testChampsPassword(): void
 	{
-		$response = $this->post('/inscription', [
-			'nom' => 'Test user',
-			'email' => 'email@test.fr',
-			'password' => null,
-			'email_verified_at' => '2023-06-06 06:06:06',
-			'password_confirmation' => 'password',
-		]);
+		$this->donnees_user['password'] = null;
+
+		$response = $this->post('/inscription', $this->donnees_user);
 
 		$response->assertStatus(302);
 	}
 
 	public function testLongueurMinimumPassword(): void
 	{
-		$response = $this->post('/inscription', [
-			'nom' => 'Test user',
-			'email' => 'email@test.fr',
-			'password' => 'p',
-			'email_verified_at' => '2023-06-06 06:06:06',
-			'password_confirmation' => 'password',
-		]);
+		$this->donnees_user['password'] = 'p';
+
+		$response = $this->post('/inscription', $this->donnees_user);
 
 		$response->assertStatus(302);
 	}
 
 	public function testLongueurMaximumPassword(): void
 	{
-		$response = $this->post('/inscription', [
-			'nom' => 'Test user',
-			'email' => 'email@test.fr',
-			'password' => 'azertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuio',
-			'email_verified_at' => '2023-06-06 06:06:06',
-			'password_confirmation' => 'password',
-		]);
+		$this->donnees_user['password'] = 'azertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuioazertyuiopmlkjhgfdsqwxcvbnazertyuio';
+
+		$response = $this->post('/inscription', $this->donnees_user);
 
 		$response->assertStatus(302);
 	}
 
 	public function testChampsPasswordConfirmation(): void
 	{
+		unset($this->donnees_user['password_confirmation']);
+
 		$response = $this->post('/inscription', [
 			'nom' => 'Test user',
 			'email' => 'email@test.fr',
