cf099a4e51e2e50dbfd2342de10424f0fc31035f
drkaine <duretkevin@live.fr>
2023-07-20 22:11:25 +0200

    Front
    Mise en place d'un menu déroulant

--- resources/views/composants/menu.blade.php
+++ resources/views/composants/menu.blade.php
@@ -1,14 +1,41 @@
 <body>
     <header>
-        <a href="{{  route('accueil')  }}">Accueil</a>
-        <a href="{{  route('catalogue-recettes')  }}">Catalogue de recettes</a>
-        @auth
-            <a href="{{  route('ajout-tag')  }}">Ajout Tag</a>
-            <a href="{{  route('ajout-recette')  }}">Ajout Recette</a>
-            <a href="{{  route('mon-compte')  }}">Mon compte</a>
-            <a href="{{  route('deconnexion')  }}">Déconnexion</a>
-        @else
-            <a href="{{  route('inscription')  }}">Inscription</a>
-            <a href="{{  route('connexion')  }}">Connexion</a>
-        @endauth
+        <div class="menu-deroulant">
+            <input type="checkbox" id="checkbox-menu-deroulant" />
+            <label for="checkbox-menu-deroulant" class="icone-menu-deroulant">
+                <span class="ligne"></span>
+                <span class="ligne"></span>
+                <span class="ligne"></span>
+            </label>
+            <ul class="liste-menu-deroulant">
+                <li>
+                    <a href="{{  route('accueil')  }}">Accueil</a>
+                </li>
+                <li>
+                    <a href="{{  route('catalogue-recettes')  }}">Catalogue de recettes</a>
+                </li>
+                @auth
+                    <li>
+                        <a href="{{  route('ajout-tag')  }}">Ajout Tag</a>
+                    </li>
+                    <li>
+                        <a href="{{  route('ajout-recette')  }}">Ajout Recette</a>
+                    </li>
+                    <li>
+                        <a href="{{  route('mon-compte')  }}">Mon compte</a>
+                    </li>
+                    <li>
+                        <a href="{{  route('deconnexion')  }}">Déconnexion</a>
+                    </li>
+                @else
+                    <li>
+                        <a href="{{  route('inscription')  }}">Inscription</a>
+                    </li>
+                    <li>
+                        <a href="{{  route('connexion')  }}">Connexion</a>
+                    </li>
+                @endauth
+            </ul>
+        </div>
+
     </header>
