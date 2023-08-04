dac44ed6a44d0d21a9fdc3b8a896f32fd74bb197
drkaine <duretkevin@live.fr>
2023-08-04 19:27:36 +0000

    Fix
    Modification pipeline de déploiement

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 set -e
 
-echo "Deployment started ..."
-
-# Entrer en mode maintenance ou retourner true
-# Si le site est déjà en mode maintenance
-(php artisan down) || true
+echo "Le script commence"
+cd ~/production/repas-en-avance/ &&
 
 # Pull la dernière version de l'application.
+echo "pull origin dev"
+git config pull.rebase true
 git pull origin main
+echo "Le déploiement commence ..."
 
-# Installation des dépendances avec composer
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+# # Installation des dépendances avec composer
+# echo "composer install"
+# composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
-# Nettoyez l'ancien cache
-php artisan clear-compiled
+# # Entrer en mode maintenance ou retourner true
+# # Si le site est déjà en mode maintenance
+# # echo "(php artisan down) || true"
+# # (php artisan down) || true
 
-# Récréer le cache
-php artisan optimize
+# # Nettoyez l'ancien cache
+# echo "php artisan clear-compiled"
+# php artisan clear-compiled
 
-# Compile npm assets
-npm run prod
+# # Récréer le cache
+# echo "php artisan optimize"
+# php artisan optimize
 
-# Lancez database migrations
-php artisan migrate --force
+# # Compile npm assets
+# echo "npm run prod"
+# npm run prod
 
-# Sortir du mode maintenancer
-php artisan up
+# # Lancez database migrations
+# echo "php artisan migrate --force"
+# php artisan migrate --force
 
-echo "Deployment finished!"
+# # Sortir du mode maintenancer
+# echo "php artisan up"
+# php artisan up
+
+echo "Déploiement terminé!"
