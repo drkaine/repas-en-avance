09419e40594c7d09c0559ea76bbd5e799f2379e5
drkaine <duretkevin@live.fr>
2023-08-14 13:51:36 +0000

    Front- design
    Ajout d'un lien fontawesome
    Ajout d'icone pour le modificateur de theme
    Ajout d'un code css pour cacher le bouton

--- resources/views/composants/head.blade.php
+++ resources/views/composants/head.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Repas en avance</title>
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
 
         <script src="{{ asset('js/changeur-de-theme.js') }}"></script>
 
