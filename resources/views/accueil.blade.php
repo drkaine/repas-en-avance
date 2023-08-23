e1d67b7eb44c505b308aa2202b66cf1f10ea223e
drkaine <duretkevin@live.fr>
2023-08-23 12:09:48 +0000

    Front - Refactoring
    Mise en place d'un composant pour les cartes de recettes

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,31 +1,7 @@
 @include('composants.head')
 
 <div class="liste-des-cartes">
-    @foreach($recettes as $recette)
-        <div>
-            <div class="carte-recette">
-                <img src="{{ asset('images/recettes/carotte.jpg') }}" alt="Description de l'image"  class="image-de-la-carte">
-                <div class="texte-de-la-carte">
-                    <h2 class="titre-de-la-carte">
-                        {{  $recette->nom  }}
-                    </h2>
-                    <span>
-                        <p>
-                            Temps de préparation : {{  $recette->temps_preparation + $recette->temps_cuisson + $recette->temps_repos }} min
-                        </p>
-                    </span>
-                    
-                </div>
-            </div>
-            <div class="liste-des-ingredients">
-                @foreach($recette->recuperationIngredient as $ingredient)
-                    <div class="carte-ingredient">
-                        {{  $ingredient->nom  }}
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    @endforeach
+    @include('composants.carte_recette', ['recettes' => $recettes])
 </div>
 
 @include('composants.footer')
