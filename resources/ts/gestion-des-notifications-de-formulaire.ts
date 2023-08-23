556140c67fdb55d743d3c68c9932765251ac4013
drkaine <duretkevin@live.fr>
2023-08-23 11:30:55 +0000

    Front - Refactoring
    Ajout d'un type de sortie oublié

--- resources/ts/gestion-des-notifications-de-formulaire.ts
+++ resources/ts/gestion-des-notifications-de-formulaire.ts
@@ -1,4 +1,4 @@
-export function cacherNotificationApresDelai(span_id: string, delai: number) {
+export function cacherNotificationApresDelai(span_id: string, delai: number) : void {
     const span_element = document.getElementById(span_id);
     if (span_element) {
       setTimeout(() => {
