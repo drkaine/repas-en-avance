d2189f77d88c35a517531712d22d61c0fe697d69
drkaine <duretkevin@live.fr>
2023-09-20 08:23:54 +0000

    Front
    Ajout du formilaire de changement de mot de passe

---
+++ resources/views/mot_de_passe_oublie.blade.php
@@ -0,0 +1,42 @@
+@include('composants.head')
+
+@include('composants.notification')
+
+<form method="POST" action="{{ route('mot-de-passe-oublie') }}" class="formulaire">
+    @csrf
+
+    <div class="element-formulaire">
+        <label>Adresse e-mail *</label>
+        <input id="email" type="email" name="email" value="" required autofocus>
+        @error('email')
+            <span>{{ $message }}</span>
+        @enderror
+    </div>
+
+    <div class="element-formulaire">
+        <label>Nouveau mot de passe *</label>
+        <input id="password" type="password" name="password" required>
+        @error('password')
+            <span>{{ $message }}</span>
+        @enderror
+    </div>
+
+    <div class="element-formulaire">
+        <label>Nouveau mot de passe confirmation *</label>
+        <input id="password_confirmation" type="password" name="password_confirmation" required>
+        @error('password_confirmation')
+            <span>{{ $message }}</span>
+        @enderror
+    </div>
+
+    <div class="element-formulaire">
+        <button type="submit" class="bouton-formulaire">
+            Changer le mot de passe
+        </button>
+    </div>
+
+    <span>
+        Les champs avec une * sont obligatoire
+    </span>
+</form>
+@include('composants.footer')
