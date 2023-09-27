35b004303904ad6b1272ee6f6a573be80121a707
drkaine <duretkevin@live.fr>
2023-09-27 09:23:42 +0000

    Front
    Mise en place du formulaire de récupération du compte

--- resources/views/recuperation_compte.blade.php
+++ resources/views/recuperation_compte.blade.php
@@ -1,1 +1,49 @@
+@include('composants.head')
+
+<form method="POST" action="{{ route('recuperation-compte') }}" class="formulaire">
+    @csrf
+
+    <div class="element-formulaire">
+        <label>Code *</label>
+        <input id="email_anonyme" type="email" name="email_anonyme" value="" required autofocus>
+        @error('email')
+            <span>{{ $message }}</span>
+        @enderror
+    </div>
+
+    <div class="element-formulaire">
+        <label>Adresse e-mail *</label>
+        <input id="email" type="email" name="email" value="" required autofocus>
+        @error('email')
+            <span>{{ $message }}</span>
+        @enderror
+    </div>
+
+    <div class="element-formulaire">
+        <label>Nom *</label>
+        <input id="nom" type="text" name="nom" value="" required autofocus>
+        @error('nom')
+            <span>{{ $message }}</span>
+        @enderror
+    </div>
+
+    <div class="element-formulaire">
+        <label>Mot de passe *</label>
+        <input id="password" type="password" name="password" required>
+        @error('password')
+            <span>{{ $message }}</span>
+        @enderror
+    </div>
+
+    <div class="element-formulaire">
+        <button type="submit" class="bouton-formulaire">
+            Changer le mot de passe
+        </button>
+    </div>
+
+    <span>
+        Les champs avec une * sont obligatoire
+    </span>
+</form>
+
 @include('composants.footer')
