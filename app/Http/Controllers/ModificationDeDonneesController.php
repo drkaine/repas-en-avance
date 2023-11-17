f489e1db785be1bbf9acf9ca226e190b7cdb9c4c
drkaine <duretkevin@live.fr>
2023-11-17 10:26:39 +0000

    Back - Refactoring
    Extraction en méthode de la logique de la méthode du controller

--- app/Http/Controllers/ModificationDeDonneesController.php
+++ app/Http/Controllers/ModificationDeDonneesController.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Services\ModificationUserService;
 use App\Traits\GestionDB\AjoutTrait;
 use App\Traits\GestionDB\SuppressionTrait;
@@ -19,6 +20,15 @@ class ModificationDeDonneesController extends Controller
 	{
 		$user = auth()->user();
 
+		$this->modificationUser($user, $request);
+
+		$this->suppressionRegimesAlimentaires($user->id, $request->tags_regimes_alimentaires);
+
+		return response()->json(['message' => 'modification réussie'], 201);
+	}
+
+	private function modificationUser(User $user, Request $request): void
+	{
 		$modification_user = new ModificationUserService($user);
 
 		$modification_user->modificationChamp('nom', $request->nom);
@@ -26,11 +36,12 @@ class ModificationDeDonneesController extends Controller
 		$modification_user->modificationChamp('email', $request->email);
 
 		$modification_user->sauvegarde();
+	}
 
-		$this->regimeAlimentaireParIdUser($user->id);
+	private function suppressionRegimesAlimentaires(int $id_user, array $tags_regimes_alimentaires): void
+	{
+		$this->regimeAlimentaireParIdUser($id_user);
 
-		$this->regimesAlimentaires($request->tags_regimes_alimentaires, $user->id);
-
-		return response()->json(['message' => 'modification réussie'], 201);
+		$this->regimesAlimentaires($tags_regimes_alimentaires, $id_user);
 	}
 }
