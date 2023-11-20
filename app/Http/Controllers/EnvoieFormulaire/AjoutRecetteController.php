e5ec46dd6544650474e2512504ce19b45cbf330c
drkaine <duretkevin@live.fr>
2023-11-20 12:57:58 +0000

    Back - Refactoring
    Extract méthod pour réduire la méthode du controleur

--- app/Http/Controllers/EnvoieFormulaire/AjoutRecetteController.php
+++ app/Http/Controllers/EnvoieFormulaire/AjoutRecetteController.php
@@ -21,12 +21,30 @@ class AjoutRecetteController extends Controller
 	{
 		$verification_donnee_request = new VerificationDonneeRequestService($request);
 
-		$ustensiles = $verification_donnee_request->selectMutiple('ustensiles');
-
 		$request->validate($this->recuperationDonneesAValider('ajout_recette'));
+
+		$ustensiles = $verification_donnee_request->selectMutiple('ustensiles');
 
 		$mode_de_cuissons = $verification_donnee_request->selectMutiple('mode_de_cuissons');
 
+		$this->creationDansLaDB($request, $ustensiles, $mode_de_cuissons);
+
+		$donnees_a_afficher = $this->recuperationDesDonnees();
+
+		$mode_de_cuissons = $donnees_a_afficher['mode_de_cuissons'];
+
+		$ustensiles = $donnees_a_afficher['ustensiles'];
+
+		$ingredients = $donnees_a_afficher['ingredients'];
+
+		$reponse_json = response()->json(['message' => 'Création réussie'], 201);
+		$reponse_json = json_decode($reponse_json->getContent());
+
+		return view('ajout_recette', compact('mode_de_cuissons', 'ustensiles', 'ingredients', 'reponse_json'));
+	}
+
+	private function creationDansLaDB(Request $request, array $ustensiles, array $mode_de_cuissons): void
+	{
 		$recette = $this->nouvelleRecette($request);
 
 		$this->ustensile($ustensiles, $recette->id);
@@ -38,24 +56,26 @@ class AjoutRecetteController extends Controller
 		$request->file('photos')->move(public_path('storage/images'), $recette->url . '.jpeg');
 
 		$this->nouvellePhoto($recette);
+	}
 
+	private function recuperationDesDonnees(): array
+	{
 		$recuperation_tag = new GestionAffichageService;
 
 		$id_tag_ustensiles = $recuperation_tag->premierParNom('Ustensiles');
 
-		$ustensiles = $id_tag_ustensiles->recuperationTagEnfants;
-
 		$id_tag_mode_de_cuissons = $recuperation_tag->premierParNom('Mode de cuisson');
-
-		$mode_de_cuissons = $id_tag_mode_de_cuissons->recuperationTagEnfants;
 
 		$id_tag_ingredients = $recuperation_tag->premierParNom('Ingrédients');
 
-		$ingredients = $id_tag_ingredients->recuperationTagEnfants;
+		$donnees_a_afficher = [];
 
-		$reponse_json = response()->json(['message' => 'Création réussie'], 201);
-		$reponse_json = json_decode($reponse_json->getContent());
+		$donnees_a_afficher['ustensiles'] = $id_tag_ustensiles->recuperationTagEnfants;
 
-		return view('ajout_recette', compact('mode_de_cuissons', 'ustensiles', 'ingredients', 'reponse_json'));
+		$donnees_a_afficher['mode_de_cuissons'] = $id_tag_mode_de_cuissons->recuperationTagEnfants;
+
+		$donnees_a_afficher['ingredients'] = $id_tag_ingredients->recuperationTagEnfants;
+
+		return $donnees_a_afficher;
 	}
 }
